172185ca3c873e47cf865952f090ba7891908354
JulioRobot <juliorobot16@gmail.com>
2025-05-27 16:20:33 +0700

    learning dynamic route

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Detais Product</h1>
+
+<h2>Product 1</h2>
+<h2>Product 2</h2>
+<h3>Product 3</h3>
---
+++ src/routes/products/[product_Id]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+    const product_Id = $page.params.product_Id;
+</script>
+
+<h1>Details about product {product_Id}</h1>
