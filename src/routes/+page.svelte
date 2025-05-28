ca439a79672b279201a1755fb34d4a678c5e84fa
JulioRobot <juliorobot16@gmail.com>
2025-05-28 17:00:13 +0700

    learn navigating programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,24 @@
+<script>
+    import {goto, beforeNavigate, afterNavigate} from '$app/navigation';
+
+    const handleClick = () =>{
+        console.log('Placing your order');
+        goto('/products');
+    };
+
+    beforeNavigate((navigation) => {
+        console.log({ before: navigation});   
+    });
+
+    afterNavigate((navigation) => {
+        console.log({ after: navigation});
+    });
+
+</script>
+
 <h1>WELCOME HOME</h1>
 
 <a href="/Blog">Blog</a>
 <a href="/products">Products</a>
+
+<button on:click={handleClick}>Place order</button>
